236b766cbe2a8f04081c76cb9be4c869332001ec
BuildTools <streetninja22@gmail.com>
2018-01-15 22:14:00 -0600

    Revert "Merge https://github.com/streetninja22/chessframework"
    This reverts commit 296776ea70cd062d16bd5da12733bef5d9557cc1, reversing
    changes made to e5e7fb9fc0d61efc678c2f8abbd3844884cb6e55.

---
+++ src/ChessGraphics.cpp
@@ -0,0 +1,9 @@
+//
+//  ChessGraphics.cpp
+//  Chess
+//
+//  Created by Tristan Hudiburg on 12/28/17.
+//
+//
+
+#include <stdio.h>
---
+++ src/ChessGraphics.h
@@ -0,0 +1,13 @@
+//
+//  ChessGraphics.h
+//  Chess
+//
+//  Created by Tristan Hudiburg on 12/26/17.
+//
+//
+
+#ifndef ChessGraphics_h
+#define ChessGraphics_h
+
+
+#endif /* ChessGraphics_h */
